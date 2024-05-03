3df9f2fdc3b1bb82597cef8a59014fe4af5e0c68
faryabkhan <faryabacc@gmail.com>
2024-05-03 14:39:14 +0500

    Initial commit

---
+++ Faryab.java
@@ -0,0 +1,11 @@
+      public class ali{
+        public static void main(String [] args){
+          for(int i=0;i,=10;i++){
+           System.out.println("Faryab Ali Khan");
+
+
+        }
+}
+}
+
+
